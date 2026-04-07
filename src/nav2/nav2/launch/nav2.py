4f4645cc4c3c5f0a8e7d130beb93e3e79348c731
Anirudh Chavali <anirudh110106@gmail.com>
2026-04-07 11:48:37 +0530

    path changes

--- src/nav2/nav2/launch/nav2.py
+++ src/nav2/nav2/launch/nav2.py
@@ -9,8 +9,8 @@ def generate_launch_description():
     nav2_bringup_dir = get_package_share_directory('nav2_bringup')
     nav2_launch_file = os.path.join(nav2_bringup_dir, 'launch', 'bringup_launch.py')
 
-    map_file = '/home/rpd/Nav2/nav2/nav2/my_new_map.yaml'
-    params_file = '/home/rpd/Nav2/nav2/nav2/dwb_params.yaml'
+    map_file = '/home/rpd/Nav2/src/nav2/nav2/my_new_map.yaml'
+    params_file = '/home/rpd/Nav2/src/nav2/nav2/dwb_params.yaml'
 
     return LaunchDescription([
         Node(
