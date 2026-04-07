70cf2934d693cd1b946bf9c5fe936663ab8ec56f
Anirudh Chavali <anirudh110106@gmail.com>
2026-04-07 11:21:01 +0530

    nice

--- build/my_robot/prefix_override/sitecustomize.py
+++ build/my_robot/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/rpd/Nav2/install/my_robot'
+    sys.prefix = sys.exec_prefix = '/home/anirudh/Nav2/install/my_robot'
