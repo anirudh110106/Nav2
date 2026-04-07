bd5c1ca1f156616b466785c51203ad536824debb
Anirudh Chavali <anirudh110106@gmail.com>
2026-04-07 12:59:51 +0530

    added pkg for autonomous map build

---
+++ build/explore_lite/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/anirudh/Nav2/install/explore_lite_msgs/share/explore_lite_msgs/package.sh"
---
+++ build/explore_lite_msgs/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/anirudh/Nav2/src/m-explore-ros2/explore_lite_msgs
+# Build directory: /home/anirudh/Nav2/build/explore_lite_msgs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("explore_lite_msgs__py")
