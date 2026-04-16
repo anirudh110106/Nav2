01056839275097ec1703fbf36f3b8b7d86b2dd44
Anirudh Chavali <anirudh110106@gmail.com>
2026-04-16 12:20:38 +0530

    dumb aa me

--- src/nav2/nav2/launch/slam.py
+++ src/nav2/nav2/launch/slam.py
@@ -23,14 +23,14 @@ def generate_launch_description():
             package='tf2_ros',
             executable='static_transform_publisher',
             name='base_to_laser_tf',
-            arguments=['0.1', '0.0', '0.2', '0', '0', '0', 'base_link', 'laser']
+            arguments=['0.1', '0.0', '0.2', '0.0', '0.0', '3.14159', 'base_link', 'laser']
         ),
 
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
             name='footprint_to_link_tf',
-            arguments=['0', '0', '0', '0', '0', '0', 'base_footprint', 'base_link']
+            arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', 'base_footprint', 'base_link']
         ),
 
         IncludeLaunchDescription(
